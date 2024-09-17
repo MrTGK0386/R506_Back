0e8222b48e4edc5ce2b2bd17ba6244f45cae1829
MrTGK <mrtiti30@gmail.com>
2024-09-17 08:47:55 +0200

    Add : Pokemon route and pokemon varities route

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -11,4 +11,13 @@ class PokemonController extends Controller
         return Pokemon::with(['defaultVariety', 'defaultVariety.sprites'])
             ->paginate(20);
     }
+
+    public function show(Pokemon $pokemon){
+        return $pokemon->load(['defaultVariety', 'defaultVariety.sprites','defaultVariety.types']);
+    }
+
+    public function showVarieties(Pokemon $pokemon)
+    {
+        return $pokemon->varieties()->with(['sprites', 'types'])->get();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/pokemon', [PokemonController::class, 'index']);
+Route::group(['prefix' => 'pokemon'], function () {
+    Route::get('/', [PokemonController::class, 'index']);
+    Route::get('/{pokemon}', [PokemonController::class, 'show']);
+    Route::get('/{pokemon}/varieties', [PokemonController::class, 'showVarieties']);
+});
+
